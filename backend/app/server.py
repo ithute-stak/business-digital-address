f948206ab6770e0c847cba926283edc49f493567
ithute-stak <thekoetlisi@tjekatjeka.co.ls>
2026-09-17 09:33:18 +0200

    Expose preproduction correspondence APIs

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,5 +1,6 @@
 from .health import router as health_router
 from .integration_api import router as integration_router
+from .launch_features import router as launch_features_router
 from .main import app
 from .membership_api import router as membership_router
 from .platform_config_api import router as platform_config_router
@@ -25,3 +26,4 @@ app.include_router(platform_config_router)
 app.include_router(membership_router)
 app.include_router(portal_router)
 app.include_router(integration_router)
+app.include_router(launch_features_router)
